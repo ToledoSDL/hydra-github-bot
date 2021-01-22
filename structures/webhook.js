4a014bab3b0df5fd5ee30ffcf656e178f4f5c82a
ToledoSDL <lucastoledoaraujo@hotmail.com>
2021-01-22 13:36:26 -0300

    a

--- structures/webhook.js
+++ structures/webhook.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = (client) => {
   var http = require("http");
   var createHandler = require("github-webhook-handler");
@@ -16,24 +18,48 @@ module.exports = (client) => {
   });
 
   handler.on("push", function (event) {
-    console.log(
-      "Received a push event for %s to %s",
-      event.payload.repository.name,
-      event.payload.ref
-    );
+    const embed = new MessageEmbed()
+      .setAuthor(
+        event.payload.sender.login,
+        event.payload.sender.avatar_url,
+        event.payload.sender.html_url
+      )
+      .setTitle(event.payload.repository.full_name)
+      .addField("Mensagem do commit:", event.payload.head_commit.message)
+      .setFooter(event.payload.repository.description)
+      .setTimestamp()
+      .setColor("#c078f0");
+    logger(embed);
   });
 
   handler.on("issues", function (event) {
-    console.log(
-      "Received an issue event for %s action=%s: #%d %s",
-      event.payload.repository.name,
-      event.payload.action,
-      event.payload.issue.number,
-      event.payload.issue.title
-    );
+    const embed = new MessageEmbed()
+      .setAuthor(
+        event.payload.sender.login,
+        event.payload.sender.avatar_url,
+        event.payload.sender.html_url
+      )
+      .setTitle(event.payload.repository.full_name)
+      .setDescription(
+        `${event.payload.issue.title}\n\n${event.payload.issue.body}`
+      )
+      .addField("Número da issue:", event.payload.issue.number)
+      .addField(
+        "Foi:",
+        event.payload.action === "closed" ? "Fechada" : "Aberta"
+      )
+      .setFooter(event.payload.repository.description)
+      .setTimestamp()
+      .setColor("#e8452c");
+    logger(embed);
   });
 
   handler.on("*", function (event) {
     console.log(event);
   });
+
+  function logger(message) {
+    const channel = client.channels.cache.get("751119080748220496");
+    channel.send(message);
+  }
 };
